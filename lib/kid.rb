9805fb3236fe883b728e7639f80da568e19c15b7
John Carter <johncarter352@gmail.com>
2020-04-27 03:02:10 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,7 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-clas Kid 
+class Kid 
 include Dance 
 
 attr_accessor :name
@@ -9,4 +9,5 @@ attr_accessor :name
   def initialize(name)
     @name = name
   end
+  
 end
